1901c79252ff6c20a10c52a4cbde565182309f52
Gladys Ndichu <itsgladysbrian@gmail.com>
2022-06-07 11:10:34 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+//Multiplied results will equal to 62
 
-// Write your code here
+const random = Math.floor((Math.random() * 10)+1);
+//Generates a random integer greater than 0
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3%num4;
+//Calculates remainder equal to 4
+
+const max = Math.max(12, 3, -5, 4, 20);
+//Returns highest number in the set
